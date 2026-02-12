f5d86abf54785f22fdf4bbdabc108a98e0004dac
snylonue <snylonue@gmail.com>
2026-02-12 12:39:40 +0000

    implement a uniswap v3 market

--- src/market/mod.rs
+++ src/market/mod.rs
@@ -1,5 +1,3 @@
-use ndarray::Array2;
-
 pub trait Market {
     fn arbitrage(&self, v: [f64; 2]) -> ([f64; 2], [f64; 2]);
 }
@@ -8,7 +6,8 @@ pub trait Market {
 pub struct UniswapV3 {
     current_price: f64,
     current_tick: usize,
-    lower_ticks: Vec<f64>,
+    lower_ticks: Vec<i32>,
+    // sqrt of liquidity
     liquidity: Vec<f64>,
     fee: f64,
 }
@@ -18,16 +17,74 @@ impl UniswapV3 {}
 impl Market for UniswapV3 {
     fn arbitrage(&self, v: [f64; 2]) -> ([f64; 2], [f64; 2]) {
         let p = v[0] / v[1];
+        let p0 = self.current_price.powi(2);
 
-        if self.current_price * self.fee <= p && p <= self.current_price / self.fee {
-            Default::default()
-        } else if p < self.current_price * self.fee {
+        if p < p0 * self.fee {
+            let prices = self.lower_ticks[self.current_tick..]
+                .iter()
+                .map(|&tick| 1.0001_f64.powi(tick))
+                .collect::<Vec<_>>();
+            let liquidity = &self.liquidity[self.current_tick..];
             let mut input = [0.0; 2];
             let mut output = [0.0; 2];
+            let mut initial = true;
 
-            todo!()
+            for i in 0..self.liquidity.len() - self.current_tick {
+                let k = liquidity[i];
+                if k.abs() <= f64::EPSILON {
+                    initial = false;
+                    continue;
+                }
+                let pu = prices[i];
+                let alpha = k / pu;
+                let beta = k * pu;
+                let p_cur = if i > 0 { pu } else { p0 };
+                let range =
+                    BoundedLiquidity::new(k, alpha, beta, k / p_cur - alpha, k * p_cur + beta);
+                let (delta0, delta1) = range.arbitrage_pos(p0 / self.fee);
+                if !initial && (delta0.abs() <= f64::EPSILON || delta1.abs() <= f64::EPSILON) {
+                    break;
+                }
+                initial = false;
+                input[0] += delta0;
+                output[1] += delta1;
+            }
+            input[0] /= self.fee;
+            (input, output)
+        } else if p > p0 / self.fee {
+            let prices = self.lower_ticks[1..=self.current_tick + 1]
+                .iter()
+                .map(|&tick| 1.0001_f64.powi(tick))
+                .collect::<Vec<_>>();
+            let liquidity = &self.liquidity[..=self.current_tick];
+            let mut input = [0.0; 2];
+            let mut output = [0.0; 2];
+            let mut initial = true;
+
+            for i in (0..=self.current_tick).rev() {
+                let k = liquidity[i];
+                if k.abs() <= f64::EPSILON {
+                    initial = false;
+                    continue;
+                }
+                let pl = prices[i];
+                let alpha = k / pl;
+                let beta = k * pl;
+                let p_cur = if i < self.current_tick { pl } else { p0 };
+                let range =
+                    BoundedLiquidity::new(k, beta, alpha, k * p_cur + beta, k / p_cur - alpha);
+                let (delta0, delta1) = range.arbitrage_pos(1.0 / (self.fee * p0));
+                if !initial && (delta0.abs() <= f64::EPSILON || delta1.abs() <= f64::EPSILON) {
+                    break;
+                }
+                initial = false;
+                input[1] += delta0;
+                output[0] += delta1;
+            }
+            input[1] /= self.fee;
+            (input, output)
         } else {
-            todo!()
+            Default::default()
         }
     }
 }
@@ -42,8 +99,8 @@ pub struct BoundedLiquidity {
 }
 
 impl BoundedLiquidity {
-    // SAFETY: invarient k = (r1 + alpha)(r2 + beta)
-    pub unsafe fn new_unchecked(k: f64, alpha: f64, beta: f64, r1: f64, r2: f64) -> Self {
+    pub fn new(k: f64, alpha: f64, beta: f64, r1: f64, r2: f64) -> Self {
+        debug_assert!(k - (r1 + alpha) * (r2 + beta) <= f64::EPSILON);
         Self {
             k,
             alpha,
@@ -51,11 +108,6 @@ impl BoundedLiquidity {
             r1,
             r2,
         }
-    }
-
-    pub fn new(k: f64, alpha: f64, beta: f64, r1: f64, r2: f64) -> Self {
-        assert!(k - (r1 + alpha) * (r2 + beta) <= f64::EPSILON);
-        unsafe { Self::new_unchecked(k, alpha, beta, r1, r2) }
     }
 
     pub fn arbitrage_pos(&self, p: f64) -> (f64, f64) {
